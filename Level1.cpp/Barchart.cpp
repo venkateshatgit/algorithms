82ad54d0fb96f50d5afb09441dfcb67b00fd9f83
venkateshatgit <vpgunake_b18@me.vjti.ac.in>
2021-11-03 11:20:41 +0530

    Print Barchats

---
+++ Level1.cpp/Barchart.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printBarChart(int* arr, int n, int h){
+
+    while(h>0){
+
+        for(int i=0; i<n; ++i){
+
+            if(h==arr[i]){
+                cout<<" * ";
+                arr[i]-=1;
+            }
+            else{
+                cout<<"   ";
+            }
+        }
+        cout<<endl;
+        h-=1;
+    }
+}
+
+int main(){
+
+    int n, height=INT_MIN;
+    cin>>n;
+    int* arr=new int[n];
+    for(int i=0; i<n; ++i){
+        cin>>arr[i];
+        height=max(height, arr[i]);
+    }
+        
+
+    printBarChart(arr, n, height);
+
+/*
+output
+5
+3 1 0 7 5
+        *
+        *
+        *  *
+        *  *
+*        *  *
+*        *  *
+*  *     *  *
+
+*/
+    delete [] arr;
+    return 0;
+}
