955aa227df6d03cbd2264401bf00ab5bb32f1e35
venkateshatgit <vpgunake_b18@me.vjti.ac.in>
2021-11-06 15:06:18 +0530

    Brute Fore Method

---
+++ Level2.cpp/ContainerWithMostWater.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int* arr=new int[n];
+    for(int i=0; i<n; ++i)
+        cin>>arr[i];
+    
+    int max_water=INT_MIN, water;
+    for(int i=0; i<n; ++i){
+        for(int j=i+1; j<n; ++j){
+
+            water=min(arr[i], arr[j])*(j-1);
+            max_water=max(max_water, water);
+        }
+    }
+
+    cout<<max_water<<endl;
+
+    delete [] arr;
+    return 0;
+}
