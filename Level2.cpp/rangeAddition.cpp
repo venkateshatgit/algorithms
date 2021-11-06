7a8c625170851f7832b78d79ec4856e26d020de9
venkateshatgit <vpgunake_b18@me.vjti.ac.in>
2021-11-06 14:15:48 +0530

    Optimized soln

--- Level2.cpp/rangeAddition.cpp
+++ Level2.cpp/rangeAddition.cpp
@@ -4,15 +4,23 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int* arr = new int[n];
-    for(int i=0; i<n; ++i)
+    int* arr = new int[n+1];
+    for(int i=0; i<n+1; ++i)
         arr[i]=0;
     int q, s, e, inc;
+    
     cin>>q;
     while(q--){
         cin>>s>>e>>inc;
-        for(int i=s; i<=e; ++i)
-            arr[i]+=inc;
+
+        arr[s]+=inc;
+        arr[e+1]+=(-1)*inc;
+    }
+
+    int sum=0; 
+    for(int i=0; i<n+1; ++i){
+        sum+=arr[i];
+        arr[i]=sum;
     }
 
     for(int i=0; i<n; ++i)
