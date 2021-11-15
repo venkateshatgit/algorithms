54469eadb8b46378676c8e7d9ce009d3f5ca2e81
venkateshatgit <vpgunake_b18@me.vjti.ac.in>
2021-11-15 12:03:55 +0530

    added palindrome function

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -49,10 +49,30 @@ int findMid(Node* node){
     return slow->data;
 }
 
+bool palindrome(Node* &node1, Node* node2){
+
+    if(node2 == NULL)
+        return true;
+
+    bool result = palindrome(node1, node2->next);
+
+    if(node1->data == node2->data && result){
+        node1 = node1->next;
+        return true;
+    }
+
+    return false;
+}
+
 int main(){
     Node* head = input();
     printList(head);
-    cout<<findMid(head)<<endl;
+    // cout<<findMid(head)<<endl;
+
+    if(palindrome(head, head))
+        cout<<"Is Palindrome"<<endl;
+    else 
+        cout<<"Not a Palindrome"<<endl;
 
     return 0;
 }
